566d361f47ed7a8e11524aafe5a5988e55d1bdb6
ysc2067 <ysc2067tylf@gmail.com>
2025-02-27 15:09:52 +0800

    add chatbot

---
+++ chatbot.py
@@ -0,0 +1,38 @@
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+print(config['TELEGRAM']['ACCESS_TOKEN'])
+
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+import configparser
+import logging
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    dispatcher = updater.dispatcher
+    
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+    
+    updater.start_polling()
+    updater.idle()
+
+def echo(update, context):
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=reply_message
+    )
+
+if __name__ == '__main__':
+    main()
